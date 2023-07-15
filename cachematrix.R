4d3b98b07a421bf7f809e9cf3592a35a34fbd528
Lukas Jaworski <ljaworski88@gmail.com>
2023-07-15 16:30:41 -0500

    implemented makeCacheMatrix and cacheSolve functions using cachemean template

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
+#[ Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## This function adds a cache to a matrix to see if it has been inverted
 
 makeCacheMatrix <- function(x = matrix()) {
 
+   inv <- NULL
+   set <- function(y) {
+           x <<- y
+           inv <<- NULL
+   }
+   get <- function() x
+   setinv <- function(solve) inv <<- solve
+   getinv <- function() inv
+   list(set = set, get = get,
+        setinv = setinv,
+        getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## Use the solve function on a cached matrix if it has no cached solution
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+   ## Return a matrix that is the inverse of 'x'
+   m <- x$getinv()
+   if (!is.null(m)) {
+           message("getting cached data")
+           return(m)
+   }
+   data <- x$get()
+   m <- solve(data, ...)
+   x$setinv(m)
+   m
 }
